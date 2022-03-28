34ad9dce5bf594ad41a29d8d3d6646b8981deb43
Rweinier739 <100585446+Rweinier739@users.noreply.github.com>
2022-03-28 14:30:16 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -7,93 +7,80 @@ input.onButtonPressed(Button.B, function () {
 let fruit: game.LedSprite = null
 let varken: game.LedSprite = null
 varken = game.createSprite(2, 4)
-basic.forever(function () {
-    if (game.score() == 100) {
-        basic.showString("Winner")
-    }
-})
-basic.forever(function () {
-    if (game.score() == 30) {
-        for (let index = 0; index < 4; index++) {
-            basic.pause(700)
-            fruit.change(LedSpriteProperty.Y, 1)
-        }
-    }
-})
+music.playMelody("C5 G B A F A C5 B ", 500)
 basic.forever(function () {
     fruit = game.createSprite(randint(0, 4), 0)
-    for (let index = 0; index < 4; index++) {
-        basic.pause(1000)
-        fruit.change(LedSpriteProperty.Y, 1)
-    }
-    if (fruit.isTouching(varken)) {
-        fruit.delete()
-        game.addScore(1)
-    } else {
-        game.gameOver()
-    }
-})
-basic.forever(function () {
-    if (game.score() == 90) {
+    if (game.score() >= 0 && game.score() < 10) {
         for (let index = 0; index < 4; index++) {
-            basic.pause(100)
+            basic.pause(1000)
             fruit.change(LedSpriteProperty.Y, 1)
         }
     }
-})
-basic.forever(function () {
-    if (game.score() == 10) {
+    if (game.score() >= 10 && game.score() < 20) {
         for (let index = 0; index < 4; index++) {
             basic.pause(900)
             fruit.change(LedSpriteProperty.Y, 1)
         }
     }
-})
-basic.forever(function () {
-    if (game.score() == 20) {
+    if (game.score() >= 20 && game.score() < 30) {
         for (let index = 0; index < 4; index++) {
             basic.pause(800)
             fruit.change(LedSpriteProperty.Y, 1)
         }
     }
-})
-basic.forever(function () {
-    if (game.score() == 80) {
+    if (game.score() >= 30 && game.score() < 40) {
         for (let index = 0; index < 4; index++) {
-            basic.pause(200)
+            basic.pause(700)
             fruit.change(LedSpriteProperty.Y, 1)
         }
     }
-})
-basic.forever(function () {
-    if (game.score() == 40) {
+    if (game.score() >= 40 && game.score() < 50) {
         for (let index = 0; index < 4; index++) {
             basic.pause(600)
             fruit.change(LedSpriteProperty.Y, 1)
         }
     }
-})
-basic.forever(function () {
-    if (game.score() == 50) {
+    if (game.score() >= 50 && game.score() < 60) {
         for (let index = 0; index < 4; index++) {
             basic.pause(500)
             fruit.change(LedSpriteProperty.Y, 1)
         }
     }
-})
-basic.forever(function () {
-    if (game.score() == 60) {
+    if (game.score() >= 60 && game.score() < 70) {
         for (let index = 0; index < 4; index++) {
             basic.pause(400)
             fruit.change(LedSpriteProperty.Y, 1)
         }
     }
-})
-basic.forever(function () {
-    if (game.score() == 70) {
+    if (game.score() >= 70 && game.score() < 80) {
         for (let index = 0; index < 4; index++) {
             basic.pause(300)
             fruit.change(LedSpriteProperty.Y, 1)
         }
+    }
+    if (game.score() >= 80 && game.score() < 90) {
+        for (let index = 0; index < 4; index++) {
+            basic.pause(200)
+            fruit.change(LedSpriteProperty.Y, 1)
+        }
+    }
+    if (game.score() >= 90 && game.score() < 100) {
+        for (let index = 0; index < 4; index++) {
+            basic.pause(100)
+            fruit.change(LedSpriteProperty.Y, 1)
+        }
+    }
+    if (game.score() == 100) {
+        basic.showString("Winner")
+        for (let index = 0; index < 4; index++) {
+            music.playMelody("F A C A B G A E ", 250)
+        }
+    }
+    if (fruit.isTouching(varken)) {
+        fruit.delete()
+        game.addScore(1)
+        music.playTone(784, music.beat(BeatFraction.Whole))
+    } else {
+        game.gameOver()
     }
 })
